3ba9aa3036175e57aa6f659b7d274d6f72838ce1
peterfei <peterfeispace@gmail.com>
2025-12-14 06:04:56 +0800

    修复：导致状态栏/终端消失的布局溢出问题

--- src/App.tsx
+++ src/App.tsx
@@ -81,12 +81,16 @@ function App() {
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         
-        <div className="flex-1 flex flex-col min-w-0 bg-[#1e1e1e]">
+        <div className="flex-1 flex flex-col min-w-0 bg-[#1e1e1e] overflow-hidden">
           <TabBar />
-          <div className="flex-1 relative">
+          <div className="flex-1 relative overflow-hidden">
             <MonacoEditor />
           </div>
-          {isTerminalOpen && <TerminalPanel onClose={toggleTerminal} />}
+          {isTerminalOpen && (
+            <div className="h-64 border-t border-gray-700 relative">
+              <TerminalPanel onClose={toggleTerminal} />
+            </div>
+          )}
         </div>
 
         {isChatOpen && <AIChat />}
